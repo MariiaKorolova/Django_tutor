3a27e4f168d5df7f4712895acb3d7ecf7aa2f25a
macbookair13 <masha.korolyova17@gmail.com>
2023-03-20 22:40:03 +0100

    create and delete users; json file

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-rom django.db.models import F
+from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
